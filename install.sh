b70063943ac3c7104adf88eea70eaf3465f52ddd
vvitordantas <40401677+vvitordantas@users.noreply.github.com>
2019-02-15 15:14:29 -0200

    1.1
    Ajuste de visual explicativo e acrescentada instalação do mtr

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
-
-echo UPDATING REPOSITORY and PPAs
-
+clear
+echo ************************************
+echo *   UPDATING REPOSITORY and PPAs   *
+echo ************************************
 sudo add-apt-repository ppa:serge-rider/dbeaver-ce -y
 sudo add-apt-repository ppa:webupd8team/atom -y
 sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make -y
@@ -11,12 +12,16 @@ sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/v
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
 sudo apt-get -y update
 sudo apt-get -y upgrade
+clear
+echo ************************************
+echo *          UPDATE FINISH           *
+echo ************************************
+pause 2
 
-echo UPDATE FINISH
-
-
-echo INSTALL ESSENCIAL...
-
+clear
+echo ************************************
+echo *     INSTALL ESSENCIAL APPS       *
+echo ************************************
 sudo apt-get -y install net-tools
 sudo apt-get -y install snapd
 sudo apt-get -y install snapd-xdg-open
@@ -24,16 +29,19 @@ sudo apt-get -y install ubuntu-make
 sudo apt-get -y install software-properties-common apt-transport-https wget
 sudo apt-get -y install wget ca-certificates
 sudo apt-get -y install flatpak
+clear
+echo ************************************
+echo *  FINISH INSTALL ESSENCIAL APPS   *
+echo ************************************
+pause 2
 
-echo FINISH INSTALL
-
-
-echo INSTALLING DEV TOOLS...
-
+clear
+echo ************************************
+echo *      INSTALLING DEV TOOLS        *
+echo ************************************
 sudo snap install datagrip --classic
 sudo snap install pycharm-community --classic
 sudo snap install intellij-idea-community --classic --edge
-
 sudo apt-get -y install sublime-text
 sudo apt-get -y install zsh
 sudo apt-get -y install git
@@ -53,8 +61,10 @@ sudo apt-get -y install freeradius-utils
 sudo apt-get -y install alien
 sudo apt-get -y install htop
 sudo apt-get -y install curl
-
+sudo apt-get -y install mtr
+#
 #DOCKER
+#
 sudo apt-get remove docker docker-engine docker.io
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -62,16 +72,22 @@ sudo apt-get update
 sudo apt-get -y install docker-ce
 sudo groupadd docker
 sudo usermod -aG docker $USER
-
-
+#
 #GitKraken
+#
 wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
 sudo dpkg -i gitkraken-amd64.deb
 sudo apt-get -y -f install
 sudo dpkg -i gitkraken-amd64.deb
-
+#
 #TEAMVIEWER
+#
 wget http://download.teamviewer.com/download/teamviewer_i386.deb -O teamviewer.deb
 sudo dpkg -i teamviewer.deb
 sudo apt-get -y -f install
 sudo dpkg -i teamviewer.deb
+clear
+echo ************************************
+echo *    FINISH INSTALL DEV TOOLS      *
+echo ************************************
+pause 2
