8f6b0af9bf070bcd1b8fa3af55fb02b1ac361d2c
vvitordantas <40401677+vvitordantas@users.noreply.github.com>
2019-02-18 23:18:31 -0300

    1.4
    Ajuste dos informativos

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 clear
-echo ************************************
-echo *   UPDATING REPOSITORY and PPAs   *
-echo ************************************
+echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+echo %   UPDATING REPOSITORY and PPAs   %
+echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 sudo add-apt-repository ppa:serge-rider/dbeaver-ce -y
 sudo add-apt-repository ppa:webupd8team/atom -y
 sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make -y
@@ -13,29 +13,29 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get -y update
 sudo apt-get -y upgrade
 clear
-echo ************************************
-echo *          UPDATE FINISH           *
-echo ************************************
+echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+echo %          UPDATE FINISH           %
+echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 pause 2
 
 clear
-echo ************************************
-echo *     INSTALL ESSENCIAL APPS       *
-echo ************************************
+echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+echo %     INSTALL ESSENCIAL APPS       %
+echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 sudo apt-get -y install net-tools snapd snapd-xdg-open ubuntu-make
 sudo apt-get -y install software-properties-common apt-transport-https wget
 sudo apt-get -y install wget ca-certificates
 sudo apt-get -y install flatpak
 clear
-echo ************************************
-echo *  FINISH INSTALL ESSENCIAL APPS   *
-echo ************************************
+echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+echo %  FINISH INSTALL ESSENCIAL APPS   %
+echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 pause 2
 
 clear
-echo ************************************
-echo *      INSTALLING DEV TOOLS        *
-echo ************************************
+echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+echo %      INSTALLING DEV TOOLS        %
+echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 sudo snap install datagrip --classic
 sudo snap install pycharm-community --classic
 sudo snap install intellij-idea-community --classic --edge
@@ -67,7 +67,7 @@ sudo dpkg -i teamviewer.deb
 sudo apt-get -y -f install
 sudo dpkg -i teamviewer.deb
 clear
-echo ************************************
-echo *    FINISH INSTALL DEV TOOLS      *
-echo ************************************
+echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+echo %    FINISH INSTALL DEV TOOLS      %
+echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 pause 2
