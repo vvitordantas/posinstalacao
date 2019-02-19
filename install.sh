f29f92ddda8a7c1245ac0fbf0d9cc1dac7d595aa
vvitordantas <40401677+vvitordantas@users.noreply.github.com>
2019-02-19 00:04:27 -0300

    1.5
    Inseridas variáveis para evitar repetição de comentários

--- install.sh
+++ install.sh
@@ -1,52 +1,52 @@
 #! /bin/bash
 clear
+addrepo=sudo add-apt-repository
+instalar=sudo apt-get -y install
+snap=sudo snap install
 echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-echo %   UPDATING REPOSITORY and PPAs   %
+echo %___UPDATING REPOSITORY and PPAs___%
 echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-sudo add-apt-repository ppa:serge-rider/dbeaver-ce -y
-sudo add-apt-repository ppa:webupd8team/atom -y
-sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make -y
-sudo add-apt-repository ppa:alexlarsson/flatpak -y
-sudo add-apt-repository ppa:nilarimogard/webupd8 -y
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" -y
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
-sudo apt-get -y update
-sudo apt-get -y upgrade
+$addrepo ppa:serge-rider/dbeaver-ce -y
+$addrepo ppa:webupd8team/atom -y
+$addrepo ppa:ubuntu-desktop/ubuntu-make -y
+$addrepo ppa:alexlarsson/flatpak -y
+$addrepo ppa:nilarimogard/webupd8 -y
+$addrepo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" -y
+$addrepo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
+sudo apt-get -y update && upgrade
 clear
 echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-echo %          UPDATE FINISH           %
+echo %__________UPDATE FINISH___________%
 echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 pause 2
-
 clear
 echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-echo %     INSTALL ESSENCIAL APPS       %
+echo %_____INSTALL ESSENCIAL APPS_______%
 echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-sudo apt-get -y install net-tools snapd snapd-xdg-open ubuntu-make
-sudo apt-get -y install software-properties-common apt-transport-https wget
-sudo apt-get -y install wget ca-certificates
-sudo apt-get -y install flatpak
+$instalar net-tools snapd snapd-xdg-open ubuntu-make
+$instalar software-properties-common apt-transport-https wget
+$instalar wget ca-certificates
+$instalar flatpak
 clear
 echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-echo %  FINISH INSTALL ESSENCIAL APPS   %
+echo %__FINISH INSTALL ESSENCIAL APPS___%
 echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 pause 2
-
 clear
 echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-echo %      INSTALLING DEV TOOLS        %
+echo %______INSTALLING DEV TOOLS________%
 echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-sudo snap install datagrip --classic
-sudo snap install pycharm-community --classic
-sudo snap install intellij-idea-community --classic --edge
-sudo apt-get -y install sublime-text zsh git git-core mysql-workbench code
-sudo apt-get -y install goland-go dbeaver-ce atom awscli filezilla maven python3
-sudo apt-get -y install redis openvpn freeradius-utils alien htop curl mtr traceroute 
+$snap datagrip --classic
+$snap pycharm-community --classic
+$snap intellij-idea-community --classic --edge
+$instalar sublime-text zsh git git-core mysql-workbench code
+$instalar goland-go dbeaver-ce atom awscli filezilla maven python3
+$instalar redis openvpn freeradius-utils alien htop curl mtr traceroute
 #
 #DOCKER
 #
 sudo apt-get remove docker docker-engine docker.io
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
+$instalar apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-get update
 sudo apt-get -y install docker-ce
@@ -68,6 +68,6 @@ sudo apt-get -y -f install
 sudo dpkg -i teamviewer.deb
 clear
 echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-echo %    FINISH INSTALL DEV TOOLS      %
+echo %_____FINISH INSTALL DEV TOOLS_____%
 echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-pause 2
+pause 3
