411b2b8a12d802c641cdd5f1b154f744ae70d4b4
vvitordantas <40401677+vvitordantas@users.noreply.github.com>
2019-02-26 19:41:03 -0300

    2.3
    Ajustes iniciais de LOGs, criação de arquivo para despejo

--- install.sh
+++ install.sh
@@ -30,15 +30,15 @@ clear
 echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
 echo "%   UPDATING REPOSITORY and PPAs   %"
 echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
-sudo add-apt-repository ppa:serge-rider/dbeaver-ce -y
-sudo add-apt-repository ppa:webupd8team/atom -y
-sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make -y
-sudo add-apt-repository ppa:alexlarsson/flatpak -y
-sudo add-apt-repository ppa:nilarimogard/webupd8 -y
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" -y
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
-sudo apt-get -y update
-sudo apt-get -y upgrade
+sudo add-apt-repository ppa:serge-rider/dbeaver-ce -y >> Log-`date +%Y-%m-%d`.txt
+sudo add-apt-repository ppa:webupd8team/atom -y >> Log-`date +%Y-%m-%d`.txt
+sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make -y >> Log-`date +%Y-%m-%d`.txt
+sudo add-apt-repository ppa:alexlarsson/flatpak -y >> Log-`date +%Y-%m-%d`.txt
+sudo add-apt-repository ppa:nilarimogard/webupd8 -y >> Log-`date +%Y-%m-%d`.txt
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" -y >> Log-`date +%Y-%m-%d`.txt
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y >> Log-`date +%Y-%m-%d`.txt
+sudo apt-get -y update >> Log-`date +%Y-%m-%d`.txt
+sudo apt-get -y upgrade >> Log-`date +%Y-%m-%d`.txt
 clear
 echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
 echo "%          UPDATE FINISH           %"
@@ -48,14 +48,14 @@ clear
 echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
 echo "%     INSTALL ESSENCIAL APPS       %"
 echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
-sudo apt-get -y install net-tools
-sudo apt-get -y install snapd
-sudo apt-get -y install snapd-xdg-open
-sudo apt-get -y install ubuntu-make
-sudo apt-get -y install software-properties-common apt-transport-https wget
-sudo apt-get -y install wget ca-certificates
-sudo apt-get -y reminna*
-sudo apt-get -y install flatpak
+sudo apt-get -y install net-tools >> Log-`date +%Y-%m-%d`.txt
+sudo apt-get -y install snapd >> Log-`date +%Y-%m-%d`.txt
+sudo apt-get -y install snapd-xdg-open >> Log-`date +%Y-%m-%d`.txt
+sudo apt-get -y install ubuntu-make >> Log-`date +%Y-%m-%d`.txt
+sudo apt-get -y install software-properties-common apt-transport-https wget >> Log-`date +%Y-%m-%d`.txt
+sudo apt-get -y install wget ca-certificates >> Log-`date +%Y-%m-%d`.txt
+sudo apt-get -y reminna* >> Log-`date +%Y-%m-%d`.txt
+sudo apt-get -y install flatpak >> Log-`date +%Y-%m-%d`.txt
 clear
 echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
 echo "%  FINISH INSTALL ESSENCIAL APPS   %"
@@ -66,57 +66,57 @@ echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
 echo "%       INSTALLING DEV TOOLS       %"
 echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
 #
-sudo snap install datagrip --classic
-sudo snap install pycharm-community --classic
-sudo snap install intellij-idea-community --classic --edge
+sudo snap install datagrip --classic >> Log-`date +%Y-%m-%d`.txt
+sudo snap install pycharm-community --classic >> Log-`date +%Y-%m-%d`.txt
+sudo snap install intellij-idea-community --classic --edge >> Log-`date +%Y-%m-%d`.txt
 #
-sudo apt-get -y install sublime-text
-sudo apt-get -y install zsh
-sudo apt-get -y install git
-sudo apt-get -y install git-core
-sudo apt-get -y install mysql-workbench
-sudo apt-get -y install code
-sudo apt-get -y install goland-go
-sudo apt-get -y install dbeaver-ce
-sudo apt-get -y install atom
-sudo apt-get -y install awscli
-sudo apt-get -y install filezilla
-sudo apt-get -y install maven
-sudo apt-get -y install python3
-sudo apt-get -y install redis
-sudo apt-get -y install openvpn
-sudo apt-get -y install freeradius-utils
-sudo apt-get -y install alien
-sudo apt-get -y install htop
-sudo apt-get -y install curl 
-sudo apt-get -y mtr 
-sudo apt-get -y traceroute
+sudo apt-get -y install sublime-text >> Log-`date +%Y-%m-%d`.txt
+sudo apt-get -y install zsh >> Log-`date +%Y-%m-%d`.txt
+sudo apt-get -y install git >> Log-`date +%Y-%m-%d`.txt
+sudo apt-get -y install git-core >> Log-`date +%Y-%m-%d`.txt
+sudo apt-get -y install mysql-workbench >> Log-`date +%Y-%m-%d`.txt
+sudo apt-get -y install code >> Log-`date +%Y-%m-%d`.txt
+sudo apt-get -y install goland-go >> Log-`date +%Y-%m-%d`.txt
+sudo apt-get -y install dbeaver-ce >> Log-`date +%Y-%m-%d`.txt
+sudo apt-get -y install atom >> Log-`date +%Y-%m-%d`.txt
+sudo apt-get -y install awscli >> Log-`date +%Y-%m-%d`.txt
+sudo apt-get -y install filezilla >> Log-`date +%Y-%m-%d`.txt
+sudo apt-get -y install maven >> Log-`date +%Y-%m-%d`.txt
+sudo apt-get -y install python3 >> Log-`date +%Y-%m-%d`.txt
+sudo apt-get -y install redis >> Log-`date +%Y-%m-%d`.txt
+sudo apt-get -y install openvpn >> Log-`date +%Y-%m-%d`.txt
+sudo apt-get -y install freeradius-utils >> Log-`date +%Y-%m-%d`.txt
+sudo apt-get -y install alien >> Log-`date +%Y-%m-%d`.txt
+sudo apt-get -y install htop >> Log-`date +%Y-%m-%d`.txt
+sudo apt-get -y install curl  >> Log-`date +%Y-%m-%d`.txt
+sudo apt-get -y mtr >> Log-`date +%Y-%m-%d`.txt
+sudo apt-get -y traceroute >> Log-`date +%Y-%m-%d`.txt
 #
 #DOCKER
 #
-sudo apt-get remove docker docker-engine docker.io
-sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-get update
-sudo apt-get -y install docker-ce
-sudo groupadd docker
-sudo usermod -aG docker $USER
+sudo apt-get remove docker docker-engine docker.io >> Log-`date +%Y-%m-%d`.txt
+sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common >> Log-`date +%Y-%m-%d`.txt
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - >> Log-`date +%Y-%m-%d`.txt
+sudo apt-get update >> Log-`date +%Y-%m-%d`.txt
+sudo apt-get -y install docker-ce >> Log-`date +%Y-%m-%d`.txt
+sudo groupadd docker >> Log-`date +%Y-%m-%d`.txt
+sudo usermod -aG docker $USER >> Log-`date +%Y-%m-%d`.txt
 #
 #GitKraken
 #
-wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
-sudo dpkg -i gitkraken-amd64.deb
-sudo apt-get -y -f install
-sudo dpkg -i gitkraken-amd64.deb
-rm -rf gitkraken-amd64.deb
+wget https://release.gitkraken.com/linux/gitkraken-amd64.deb >> Log-`date +%Y-%m-%d`.txt
+sudo dpkg -i gitkraken-amd64.deb >> Log-`date +%Y-%m-%d`.txt
+sudo apt-get -y -f install >> Log-`date +%Y-%m-%d`.txt
+sudo dpkg -i gitkraken-amd64.deb >> Log-`date +%Y-%m-%d`.txt
+rm -rf gitkraken-amd64.deb >> Log-`date +%Y-%m-%d`.txt
 #
 #TEAMVIEWER
 #
-wget http://download.teamviewer.com/download/teamviewer_i386.deb -O teamviewer.deb
-sudo dpkg -i teamviewer.deb
-sudo apt-get -y -f install
-sudo dpkg -i teamviewer.deb
-rm -rf teamviewer.deb
+wget http://download.teamviewer.com/download/teamviewer_i386.deb -O teamviewer.deb >> Log-`date +%Y-%m-%d`.txt
+sudo dpkg -i teamviewer.deb >> Log-`date +%Y-%m-%d`.txt
+sudo apt-get -y -f install >> Log-`date +%Y-%m-%d`.txt
+sudo dpkg -i teamviewer.deb >> Log-`date +%Y-%m-%d`.txt
+rm -rf teamviewer.deb >> Log-`date +%Y-%m-%d`.txt
 clear
 echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
 echo "%     FINISH INSTALL DEV TOOLS     %"
